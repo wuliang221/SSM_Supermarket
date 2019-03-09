9053db6490497a933942b4bd38c6b382708c822b
DELL <DELL@DESKTOP-IF9CJNG>
2019-03-09 15:34:29 +0800

    User表更改

--- SSM_Supermarket/src/com/dao/UserMapper.java
+++ SSM_Supermarket/src/com/dao/UserMapper.java
@@ -1,5 +1,7 @@
 package com.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import com.pojo.User;
@@ -10,8 +12,13 @@ public interface UserMapper {
 	public User user(@Param("UserName")String UserName);
 	//匹配账号密码是否有误
 	public int getUser(@Param("UserName")String UserName,@Param("UserPW")String UserPW);
-	
+	//查询员工总数
 	public int userCount();
+	//查询所有的员工信息
+	public List<User> userAll(@Param("UserName")String UserName,@Param("UserStyle")Integer UserStyle,@Param("pageNo")String pageNo,
+								@Param("pageSize")String pageSize);
+	
+	public int updateUser(@Param("UserName")String UserName,@Param("UserStyle")Integer UserStyle,@Param("userPW")String userPW);
 	
 	
 	
